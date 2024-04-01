7b1a493d4caa352adc895b3d88c741c9f3d1cdc0
dldesara <dldesarazu@gmail.com>
2024-03-31 18:44:08 -0700

    new file:   .gitignore 	new file:   Cargo.lock 	new file:   Cargo.toml 	new file:   songs.json 	new file:   src/main.rs

---
+++ src/main.rs
@@ -0,0 +1,358 @@
+use libp2p::{
+    core::upgrade,
+    floodsub::{Floodsub, FloodsubEvent, Topic},
+    futures::StreamExt,
+    identity,
+    mdns::{Mdns, MdnsEvent},
+    mplex,
+    noise::{Keypair, NoiseConfig, X25519Spec},
+    swarm::{NetworkBehaviourEventProcess, Swarm, SwarmBuilder},
+    tcp::TokioTcpConfig,
+    NetworkBehaviour, PeerId, Transport,
+};
+use log::{error, info};
+use once_cell::sync::Lazy;
+use serde::{Deserialize, Serialize};
+use std::collections::HashSet;
+use tokio::{fs, io::AsyncBufReadExt, sync::mpsc};
+
+const STORAGE_FILE_PATH: &str = "./songs.json";
+
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync + 'static>>;
+type Songs = Vec<SongInfo>;
+
+static KEYS: Lazy<identity::Keypair> = Lazy::new(|| identity::Keypair::generate_ed25519());
+static PEER_ID: Lazy<PeerId> = Lazy::new(|| PeerId::from(KEYS.public()));
+static TOPIC: Lazy<Topic> = Lazy::new(|| Topic::new("lyrics"));
+
+
+#[derive(Debug, Serialize, Deserialize)]
+struct SongInfo {
+    id: usize,
+    name: String,
+    lyric: String,
+	Year: String,
+    Artist: String,
+    public: bool,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+enum ListMode {
+    ALL,
+    One(String),
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct ListRequest {
+    mode: ListMode,
+}
+
+
+#[derive(Debug, Serialize, Deserialize)]
+struct ListResponse {
+    mode: ListMode,
+    data: Songs,
+    receiver: String,
+}
+
+enum EventType {
+    Response(ListResponse),
+    Input(String),
+}
+
+
+#[derive(NetworkBehaviour)]
+struct SongBehaviour {
+    floodsub: Floodsub,
+    mdns: Mdns,
+    #[behaviour(ignore)]
+    response_sender: mpsc::UnboundedSender<ListResponse>,
+}
+
+
+impl NetworkBehaviourEventProcess<FloodsubEvent> for SongBehaviour {
+    fn inject_event(&mut self, event: FloodsubEvent) {
+        match event {
+            FloodsubEvent::Message(msg) => {
+                if let Ok(resp) = serde_json::from_slice::<ListResponse>(&msg.data) {
+                    if resp.receiver == PEER_ID.to_string() {
+                        info!("Response from {}:", msg.source);
+                        resp.data.iter().for_each(|r| info!("{:?}", r));
+                    }
+                } else if let Ok(req) = serde_json::from_slice::<ListRequest>(&msg.data) {
+                    match req.mode {
+                        ListMode::ALL => {
+                            info!("Received ALL req: {:?} from {:?}", req, msg.source);
+                            respond_with_public_songs(
+                                self.response_sender.clone(),
+                                msg.source.to_string(),
+                            );
+                        }
+                        ListMode::One(ref peer_id) => {
+                            if peer_id == &PEER_ID.to_string() {
+                                info!("Received req: {:?} from {:?}", req, msg.source);
+                                respond_with_public_songs(
+                                    self.response_sender.clone(),
+                                    msg.source.to_string(),
+                                );
+                            }
+                        }
+                    }
+                }
+            }
+            _ => (),
+        }
+    }
+}
+
+
+
+
+fn respond_with_public_songs(sender: mpsc::UnboundedSender<ListResponse>, receiver: String) {
+    tokio::spawn(async move {
+        match read_local_songs().await {
+            Ok(lyrics) => {
+                let resp = ListResponse {
+                    mode: ListMode::ALL,
+                    receiver,
+                    data: lyrics.into_iter().filter(|r| r.public).collect(),
+                };
+                if let Err(e) = sender.send(resp) {
+                    error!("error sending response via channel, {}", e);
+                }
+            }
+            Err(e) => error!("error fetching local lyrics to answer ALL request, {}", e),
+        }
+    });
+}
+
+impl NetworkBehaviourEventProcess<MdnsEvent> for SongBehaviour {
+    fn inject_event(&mut self, event: MdnsEvent) {
+        match event {
+            MdnsEvent::Discovered(discovered_list) => {
+                for (peer, _addr) in discovered_list {
+                    self.floodsub.add_node_to_partial_view(peer);
+                }
+            }
+            MdnsEvent::Expired(expired_list) => {
+                for (peer, _addr) in expired_list {
+                    if !self.mdns.has_node(&peer) {
+                        self.floodsub.remove_node_from_partial_view(&peer);
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+async fn create_new_songinfo(name: &str, lyric: &str, Year: &str, Artist: &str) -> Result<()> {
+    let mut local_songs = read_local_songs().await?;
+    let new_id = match local_songs.iter().max_by_key(|r| r.id) {
+        Some(v) => v.id + 1,
+        None => 0,
+    };
+    local_songs.push(SongInfo {
+        id: new_id,
+        name: name.to_owned(),
+        lyric: lyric.to_owned(),
+        Year: Year.to_owned(),
+		Artist: Artist.to_owned(),
+        public: false,
+    });
+    write_local_songs(&local_songs).await?;
+
+    info!("Created song:");
+    info!("Name: {}", name);
+    info!("lyric: {}", lyric);
+    info!("Year:: {}", Year);
+	info!("Artist: {}", Artist);
+
+    Ok(())
+}
+
+async fn publish_songs(id: usize) -> Result<()> {
+    let mut local_songs = read_local_songs().await?;
+    local_songs
+        .iter_mut()
+        .filter(|r| r.id == id)
+        .for_each(|r| r.public = true);
+    write_local_songs(&local_songs).await?;
+    Ok(())
+}
+
+
+async fn read_local_songs() -> Result<Songs> {
+    let content = fs::read(STORAGE_FILE_PATH).await?;
+    let result = serde_json::from_slice(&content)?;
+    Ok(result)
+}
+
+
+async fn write_local_songs(lyrics: &Songs) -> Result<()> {
+    let json = serde_json::to_string(&lyrics)?;
+    fs::write(STORAGE_FILE_PATH, &json).await?;
+    Ok(())
+}
+
+
+#[tokio::main]
+async fn main() {
+    println!("Hello, world!");
+	
+	pretty_env_logger::init();
+
+    info!("Peer Id: {}", PEER_ID.clone());
+    let (response_sender, mut response_rcv) = mpsc::unbounded_channel();
+
+    let auth_keys = Keypair::<X25519Spec>::new()
+        .into_authentic(&KEYS)
+        .expect("can create auth keys");
+	let transp = TokioTcpConfig::new()
+        .upgrade(upgrade::Version::V1)
+        .authenticate(NoiseConfig::xx(auth_keys).into_authenticated())
+        .multiplex(mplex::MplexConfig::new())
+        .boxed();
+	let mut behaviour = SongBehaviour {
+        floodsub: Floodsub::new(PEER_ID.clone()),
+        mdns: Mdns::new(Default::default())
+            .await
+            .expect("can create mdns"),
+        response_sender,
+    };
+
+    behaviour.floodsub.subscribe(TOPIC.clone());
+	
+	let mut swarm = SwarmBuilder::new(transp, behaviour, PEER_ID.clone())
+        .executor(Box::new(|fut| {
+            tokio::spawn(fut);
+        }))
+        .build();
+	let mut stdin = tokio::io::BufReader::new(tokio::io::stdin()).lines();
+	
+	Swarm::listen_on(
+		&mut swarm,
+        "/ip4/0.0.0.0/tcp/0"
+            .parse()
+            .expect("can get a local socket"),
+    )
+    .expect("swarm can be started");
+	
+	loop {
+        let evt = {
+            tokio::select! {
+                line = stdin.next_line() => Some(EventType::Input(line.expect("can get line").expect("can read line from stdin"))),
+				response = response_rcv.recv() => Some(EventType::Response(response.expect("response exists"))),
+                event = swarm.next() => {
+                    info!("Unhandled Swarm Event: {:?}", event);
+                    None
+                },
+            }
+        };
+         if let Some(event) = evt {
+            match event {
+                EventType::Response(resp) => {
+                    let json = serde_json::to_string(&resp).expect("can jsonify response");
+                    swarm
+                        .behaviour_mut()
+                        .floodsub
+                        .publish(TOPIC.clone(), json.as_bytes());
+                }
+                EventType::Input(line) => match line.as_str() {
+                    "ls p" => handle_list_peers(&mut swarm).await,
+                    cmd if cmd.starts_with("ls r") => handle_list_songs(cmd, &mut swarm).await,
+                    cmd if cmd.starts_with("create r") => handle_create_songinfo(cmd).await,
+                    cmd if cmd.starts_with("publish r") => handle_publish_songinfo(cmd).await,
+                    _ => error!("unknown command"),
+                },
+            }
+        }
+    }
+}
+
+
+
+	
+async fn handle_list_peers(swarm: &mut Swarm<SongBehaviour>) {
+    info!("Discovered Peers:");
+	let nodes = swarm.behaviour().mdns.discovered_nodes();
+    let mut unique_peers = HashSet::new();
+    for peer in nodes {
+        unique_peers.insert(peer);
+    }
+    unique_peers.iter().for_each(|p| info!("{}", p));
+}
+
+async fn handle_list_songs(cmd: &str, swarm: &mut Swarm<SongBehaviour>) {
+    let rest = cmd.strip_prefix("ls r ");
+    match rest {
+        Some("all") => {
+            let req = ListRequest {
+                mode: ListMode::ALL,
+            };
+            let json = serde_json::to_string(&req).expect("can jsonify request");
+            swarm
+                .behaviour_mut()
+                .floodsub
+                .publish(TOPIC.clone(), json.as_bytes());
+        }
+        Some(songs_peer_id) => {
+            let req = ListRequest {
+                mode: ListMode::One(songs_peer_id.to_owned()),
+            };
+            let json = serde_json::to_string(&req).expect("can jsonify request");
+            swarm
+                .behaviour_mut()
+                .floodsub
+                .publish(TOPIC.clone(), json.as_bytes());
+        }
+        None => {
+            match read_local_songs().await {
+                Ok(v) => {
+                    info!("Local Songs ({})", v.len());
+                    v.iter().for_each(|r| info!("{:?}", r));
+                }
+                Err(e) => error!("error fetching local lyrics: {}", e),
+            };
+        }
+    };
+}
+
+
+
+
+async fn handle_create_songinfo(cmd: &str) {
+    if let Some(rest) = cmd.strip_prefix("create r") {
+        let elements: Vec<&str> = rest.split("|").collect();
+        if elements.len() < 4 {
+            info!("too few arguments - Format: name|ingredients|instructions");
+        } else {
+            let name = elements.get(0).expect("name is there");
+            let lyric = elements.get(1).expect("Iconic lyric is there");
+            let Year = elements.get(2).expect("Year is there");
+			let Artist = elements.get(3).expect("Artist is there");
+            if let Err(e) = create_new_songinfo(name, lyric, Year, Artist).await {
+                error!("error creating song info: {}", e);
+            };
+        }
+    }
+}
+
+async fn handle_publish_songinfo(cmd: &str) {
+    if let Some(rest) = cmd.strip_prefix("publish r") {
+        match rest.trim().parse::<usize>() {
+            Ok(id) => {
+                if let Err(e) = publish_songs(id).await {
+                    info!("error publishing song info with id {}, {}", id, e)
+                } else {
+                    info!("Published SongInfo with id: {}", id);
+                }
+            }
+            Err(e) => error!("invalid id: {}, {}", rest.trim(), e),
+        };
+    }
+}
+
+
+
+
